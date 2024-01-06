0714b7754a4688ac4c6897620ac51c32b7f96a63
Ninad More <ninadyogeshmore@gmail.com>
2024-01-06 11:08:45 +0530

    tp

---
+++ lib/features/sub_events/widgets/events_card_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EventCardWidget extends StatelessWidget {
+  const EventCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: Stack(
+        children: [],
+      ),
+    );
+  }
+}
